236a916f7505d91740818e25c0668faaf99a21f6
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-01 18:52:11 +0100

    refactor(Day1): Change part 2 to looping
    Iterative looks cleaner to me here.
    Bit weird that I need the while (true) because kotlin doesn't like old style for loops.

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day01.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day01.kt
@@ -1,15 +1,19 @@
 package nl.kelpin.fleur.advent2018
 
 class Day01(input: List<String>) {
-    private val ints = input.map(String::toInt)
+    private val deltas = input.map(String::toInt)
 
-    fun part1(): Int = ints.sum()
+    fun part1(): Int = deltas.sum()
 
-    tailrec fun part2(encountered: MutableSet<Int> = mutableSetOf(0), frequency: Int = 0, index: Int = 0): Int {
-        val newFrequency: Int = frequency + ints[index]
-        return if (!encountered.add(newFrequency))
-            newFrequency
-        else
-            part2(encountered, newFrequency, (index + 1).rem(ints.size))
+    fun part2(): Int {
+        val encounteredFrequencies = mutableSetOf<Int>()
+        var currentFrequency = 0
+        while (true) {
+            for (delta in deltas) {
+                val alreadyEncountered = !encounteredFrequencies.add(currentFrequency)
+                if (alreadyEncountered) return currentFrequency
+                currentFrequency += delta
+            }
+        }
     }
 }
