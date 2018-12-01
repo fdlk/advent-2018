e73e4b70a1cea2034006c86db23400982d27fe7b
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-01 18:30:59 +0100

    fix(Day1): You've seen 0 so include it in the set

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day01.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day01.kt
@@ -5,7 +5,7 @@ class Day01(input: List<String>) {
 
     fun part1(): Int = ints.sum()
 
-    tailrec fun part2(encountered: MutableSet<Int> = mutableSetOf(), frequency: Int = 0, index: Int = 0): Int {
+    tailrec fun part2(encountered: MutableSet<Int> = mutableSetOf(0), frequency: Int = 0, index: Int = 0): Int {
         val newFrequency: Int = frequency + ints[index]
         return if (!encountered.add(newFrequency))
             newFrequency
