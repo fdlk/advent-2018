e71bde6ff8fc3686c036fdcc1cd0f2eeddd19792
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-03 09:28:33 +0100

    refactor(Day03): Put claim parse method in Claim companion object

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day03.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day03.kt
@@ -1,14 +1,15 @@
 package nl.kelpin.fleur.advent2018
 
-class Day03(input: List<String>) {
-    companion object {
-        fun parse(claimLine: String): Claim {
-            val values = (Regex("""(#\d+) @ (\d+),(\d+): (\d+)x(\d+)""").matchEntire(claimLine))!!.groupValues
-            return Claim(values[1], values[2].toInt(), values[3].toInt(), values[4].toInt(), values[5].toInt())
-        }
-    }
+import nl.kelpin.fleur.advent2018.Day03.Claim.Companion.parse
 
+class Day03(input: List<String>) {
     data class Claim(val id: String, val topLeftX: Int, val topLeftY: Int, val width: Int, val height: Int) {
+        companion object {
+            fun parse(claimLine: String): Claim {
+                val values = (Regex("""(#\d+) @ (\d+),(\d+): (\d+)x(\d+)""").matchEntire(claimLine))!!.groupValues
+                return Claim(values[1], values[2].toInt(), values[3].toInt(), values[4].toInt(), values[5].toInt())
+            }
+        }
         val xRange = topLeftX until topLeftX + width
         val yRange = topLeftY until topLeftY + height
         fun contains(x: Int, y: Int): Boolean = xRange.contains(x) && yRange.contains(y)
--- src/test/kotlin/nl/kelpin/fleur/advent2018/Day03Test.kt
+++ src/test/kotlin/nl/kelpin/fleur/advent2018/Day03Test.kt
@@ -8,22 +8,23 @@ class Day03Test {
 
     @Test()
     fun `Parses input correctly`() {
-        Assertions.assertThat(Day03.parse("#1 @ 935,649: 22x22"))
+        Assertions.assertThat(Day03.Claim.parse("#1 @ 935,649: 22x22"))
                 .isEqualTo(Day03.Claim("#1", 935, 649, 22, 22))
     }
 
     @Test()
     fun `Claim has correct ranges`() {
-        Assertions.assertThat(Day03.parse("#123 @ 3,2: 5x4").xRange).isEqualTo(3 until 8)
-        Assertions.assertThat(Day03.parse("#123 @ 3,2: 5x4").yRange).isEqualTo(2 until 6)
+        Assertions.assertThat(Day03.Claim.parse("#123 @ 3,2: 5x4").xRange).isEqualTo(3 until 8)
+        Assertions.assertThat(Day03.Claim.parse("#123 @ 3,2: 5x4").yRange).isEqualTo(2 until 6)
     }
 
     @Test()
     fun `Claim includes is correct`() {
-        Assertions.assertThat(Day03.parse("#123 @ 3,2: 5x4").contains(2, 1)).isFalse()
-        Assertions.assertThat(Day03.parse("#123 @ 3,2: 5x4").contains(3, 2)).isTrue()
-        Assertions.assertThat(Day03.parse("#123 @ 3,2: 5x4").contains(7, 5)).isTrue()
-        Assertions.assertThat(Day03.parse("#123 @ 3,2: 5x4").contains(8, 6)).isFalse()
+        val claim = Day03.Claim.parse("#123 @ 3,2: 5x4")
+        Assertions.assertThat(claim.contains(2, 1)).isFalse()
+        Assertions.assertThat(claim.contains(3, 2)).isTrue()
+        Assertions.assertThat(claim.contains(7, 5)).isTrue()
+        Assertions.assertThat(claim.contains(8, 6)).isFalse()
     }
 
     @Test()
