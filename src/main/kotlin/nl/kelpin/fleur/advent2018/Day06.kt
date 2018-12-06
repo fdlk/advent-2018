8277bfa72f80b872ee9a896c737880f91da6245d
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-06 20:10:15 +0100

    feat(Day06): part 1

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day06.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day06.kt
@@ -1,10 +1,42 @@
 package nl.kelpin.fleur.advent2018
 
 class Day06(input: List<String>) {
-    data class Point(val x: Int, val y: Int) {
-        constructor(list: List<Int>) : this(list[0], list[1])
-    }
+    fun List<Int>.range(): IntRange = min()!! - 1..max()!! + 1
 
     val points: List<Point> = input.map { Point(it.split(",").map { it.trim().toInt() }) }
+    val xRange: IntRange = points.map { it.x }.range()
+    val yRange: IntRange = points.map { it.y }.range()
 
+    fun closestPoint(p: Point): Point? {
+        val (a, b) = points.sortedBy { it.distanceTo(p) }.take(2)
+        return if (a.distanceTo(p) == b.distanceTo(p)) null else a
+    }
+
+    fun edgePoints(): Set<Point> {
+        val bottomAndTop = xRange.flatMap { x ->
+            setOf(
+                    closestPoint(Point(x, yRange.start)),
+                    closestPoint(Point(x, yRange.endInclusive))
+            )
+        }.filterNotNull().toSet()
+        val leftAndRight = yRange.flatMap { y ->
+            setOf(
+                    closestPoint(Point(xRange.start, y)),
+                    closestPoint(Point(xRange.endInclusive, y))
+            )
+        }.filterNotNull().toSet()
+        return bottomAndTop.union(leftAndRight)
+    }
+
+
+    fun part1(): Int? {
+        val edgePoints:Set<Point> = edgePoints()
+        return xRange.flatMap { x -> yRange.map { y -> closestPoint(Point(x, y)) } }
+                .filterNotNull()
+                .filter{!edgePoints.contains(it)}
+                .groupingBy { it }
+                .eachCount()
+                .values
+                .max()
+    }
 }
--- src/test/kotlin/nl/kelpin/fleur/advent2018/Day06Test.kt
+++ src/test/kotlin/nl/kelpin/fleur/advent2018/Day06Test.kt
@@ -5,16 +5,51 @@ import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 class Day06Test {
+    companion object {
+        val sample = Day06(resourceAsList("day06-sample.txt"))
+        val actual = Day06(resourceAsList("day06.txt"))
+    }
+
     @Test
     fun `Loads default input`() {
-        assertThat(Day06(resourceAsList("day06-sample.txt")).points)
-                .isEqualTo(listOf(
-                        Day06.Point(1, 1),
-                        Day06.Point(1, 6),
-                        Day06.Point(8, 3),
-                        Day06.Point(3, 4),
-                        Day06.Point(5,5),
-                        Day06.Point(8, 9)
-                ))
+        assertThat(sample.points).isEqualTo(listOf(
+                Point(1, 1),
+                Point(1, 6),
+                Point(8, 3),
+                Point(3, 4),
+                Point(5, 5),
+                Point(8, 9)
+        ))
+    }
+
+    @Test
+    fun `ranges`() {
+        assertThat(sample.xRange).isEqualTo(0..9)
+        assertThat(sample.yRange).isEqualTo(0..10)
+    }
+
+    @Test
+    fun `manhattan distance is defined properly`() {
+        val a = Point(1, 1)
+        val b = Point(8, 9)
+        assertThat(a.distanceTo(b)).isEqualTo(15)
+        assertThat(b.distanceTo(a)).isEqualTo(15)
+    }
+
+    @Test
+    fun `closest point`() {
+        assertThat(sample.closestPoint(Point(3,2))).isEqualTo(Point(3, 4))
+        assertThat(sample.closestPoint(Point(0,4))).isNull()
+    }
+
+    @Test
+    fun `Part 1 sample data`() {
+        assertThat(sample.part1()).isEqualTo(17)
+    }
+
+    @Tag("Solution")
+    @Test
+    fun `Part 1 solution`() {
+        assertThat(actual.part1()).isEqualTo(17)
     }
 }
