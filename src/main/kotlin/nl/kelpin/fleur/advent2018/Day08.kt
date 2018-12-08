c72bfc3f41ee61bac6d8c7b20b87b284059348d8
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-08 16:38:39 +0100

    refactor(Day08): Ditch all the index magic, use an iterator instead

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day08.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day08.kt
@@ -1,29 +1,21 @@
 package nl.kelpin.fleur.advent2018
 
 class Day08(inputString: String) {
-    val input: List<Int> = inputString.split(" ").map { it.trim().toInt() }
-    val tree: Node = constructNode(0).second
+    val input: Iterator<Int> = inputString.split(" ").map { it.trim().toInt() }.asSequence().iterator()
+    val tree: Node = constructNode()
 
     data class Node(val children: List<Node>, val metadatas: List<Int>) {
         fun metadataSum(): Int = metadatas.sum() + children.sumBy { it.metadataSum() }
         fun value(): Int = if (children.isEmpty()) metadatas.sum()
-        else metadatas.sumBy { children.getOrNull(it - 1)?.value() ?: 0 }
+        else metadatas.map { children.getOrNull(it - 1) }.filterNotNull().map { it.value() }.sum()
     }
 
-    fun constructChild(soFar: Pair<Int, List<Node>>, childIndex: Int): Pair<Int, List<Node>> {
-        val (nextStartIndex, node) = constructNode(soFar.first)
-        return Pair(nextStartIndex, soFar.second + node)
-    }
-
-    fun constructNode(startIndex: Int): Pair<Int, Node> {
-        val (numberOfChildren, numberOfMetadatas) = input.drop(startIndex)
-        val (nextStartIndex: Int, children: List<Node>) = (1..numberOfChildren).fold(
-                Pair(startIndex + 2, emptyList()),
-                ::constructChild
-        )
-        val nodeEndPos = nextStartIndex + numberOfMetadatas
-        val metadatas = input.subList(nextStartIndex, nodeEndPos)
-        return Pair(nodeEndPos, Node(children, metadatas))
+    fun constructNode(): Node {
+        val numberOfChildren = input.next()
+        val numberOfMetadatas = input.next()
+        val children = (1..numberOfChildren).map { constructNode() }
+        val metadatas = (1..numberOfMetadatas).map { input.next() }
+        return Node(children, metadatas)
     }
 
     fun part1(): Int = tree.metadataSum()
