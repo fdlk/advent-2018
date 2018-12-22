1d0fa7b75e99b222e0e9fb9ff9978d040fbf9593
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-22 15:47:19 +0100

    feat(Day22): Part 2

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day22.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day22.kt
@@ -2,26 +2,34 @@ package nl.kelpin.fleur.advent2018
 
 import arrow.syntax.function.memoize
 
-sealed class Type
-object Rocky : Type() {
-    override fun toString(): String {
-        return "Rocky"
-    }
+sealed class Type(private val name: String) {
+    abstract fun isCompatibleWith(tool: Tool): Boolean
+    override fun toString(): String = name
 }
 
-object Wet : Type() {
-    override fun toString(): String {
-        return "Wet"
-    }
+object Rocky : Type("Rocky") {
+    override fun isCompatibleWith(tool: Tool) = tool != Neither
 }
 
-object Narrow : Type() {
-    override fun toString(): String {
-        return "Narrow"
-    }
+object Wet : Type("Wet") {
+    override fun isCompatibleWith(tool: Tool) = tool != Torch
 }
 
-class Day22(val depth: Int, val target: Point) {
+object Narrow : Type("Narrow") {
+    override fun isCompatibleWith(tool: Tool) = tool != ClimbingGear
+}
+
+sealed class Tool(private val name: String) {
+    override fun toString(): String = name
+}
+
+object Neither : Tool("Neither")
+object ClimbingGear : Tool("Climbing Gear")
+object Torch : Tool("Torch")
+
+data class State(val point: Point, val tool: Tool)
+
+class Day22(val depth: Int, val target: Point) : Grid<State> {
     companion object {
         val mouth = Point(0, 0)
     }
@@ -50,4 +58,33 @@ class Day22(val depth: Int, val target: Point) {
     fun part1(): Int = (mouth.y..target.y).flatMap { y ->
         (mouth.x..target.x).map { x -> Point(x, y) }
     }.sumBy(::risk)
+
+    private val costToMove = 1
+    private val costToSwitchTool = 7
+
+    override fun heuristicDistance(from: State, to: State): Int =
+            from.point.distanceTo(to.point) * costToMove +
+                    if (from.tool != to.tool) costToSwitchTool else 0
+
+    fun getNeighbours(point: Point): List<Point> = with(point) {
+        listOf(move(Down), move(Right), move(Left), move(Up))
+                .filter { it.x >= 0 && it.y >= 0 }
+    }
+
+    override fun getNeighbours(state: State): List<State> = with(state) {
+        val result = mutableListOf<State>()
+        result.addAll(getNeighbours(point)
+                .filter { type(it).isCompatibleWith(tool) }
+                .map { state.copy(point = it) })
+        result.addAll(listOf(Neither, ClimbingGear, Torch)
+                .filter { it != tool }
+                .filter { type(point).isCompatibleWith(it) }
+                .map { state.copy(tool = it) })
+        return result
+    }
+
+    override fun moveCost(from: State, to: State): Int =
+            if (from.tool == to.tool) costToMove else costToSwitchTool
+
+    fun part2(): Int = aStarSearch(State(mouth, Torch), State(target, Torch), this)
 }
--- src/test/kotlin/nl/kelpin/fleur/advent2018/Day22Test.kt
+++ src/test/kotlin/nl/kelpin/fleur/advent2018/Day22Test.kt
@@ -5,16 +5,26 @@ import org.junit.jupiter.api.Test
 
 class Day22Test {
     companion object {
-        val day22 = Day22(11739, Point(11, 718))
+        val actual = Day22(11739, Point(11, 718))
         val sample = Day22(510, Point(10, 10))
     }
     @Test
-    fun sampleRisk(){
+    fun `Sample part 1`(){
         assertThat(sample.part1()).isEqualTo(114)
     }
 
     @Test
-    fun actualRisk(){
-        assertThat(day22.part1()).isEqualTo(114)
+    fun `Solution part 1`(){
+        assertThat(actual.part1()).isEqualTo(114)
+    }
+
+    @Test
+    fun `Sample route`(){
+        assertThat(sample.part2()).isEqualTo(45)
+    }
+
+    @Test
+    fun `Solution part 2`(){
+        assertThat(actual.part2()).isEqualTo(984)
     }
 }
