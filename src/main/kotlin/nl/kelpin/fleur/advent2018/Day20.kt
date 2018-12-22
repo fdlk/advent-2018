5491450dbf67ce393212d8b4ff424a5cd024d323
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-22 01:00:20 +0100

    refactor(Day20): Ditch the parser, use a stack to remember the rooms

--- src/main/kotlin/nl/kelpin/fleur/advent2018/Day20.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/Day20.kt
@@ -1,76 +1,40 @@
 package nl.kelpin.fleur.advent2018
 
-import com.github.h0tk3y.betterParse.combinators.*
-import com.github.h0tk3y.betterParse.grammar.Grammar
-import com.github.h0tk3y.betterParse.grammar.parseToEnd
-import com.github.h0tk3y.betterParse.grammar.parser
-import com.github.h0tk3y.betterParse.parser.Parser
-
-sealed class DungeonNode
-data class Alternatives(val options: List<DungeonNode>) : DungeonNode()
-data class Moves(val moves: String) : DungeonNode() {
-    fun opposite(c: Char): Char = when (c) {
-        'N' -> 'S'
-        'E' -> 'W'
-        'S' -> 'N'
-        'W' -> 'E'
-        else -> throw IllegalStateException()
-    }
-
-    fun plus(c: Char) = when {
-        moves.endsWith(opposite(c)) -> Moves(moves.dropLast(1))
-        else -> Moves(moves + c)
-    }
-}
+import java.util.*
 
 class Day20 {
-    val TreeParser = object : Grammar<Alternatives>() {
-        val regexStart = token("""\^""")
-        val regexEnd = token("""\$""")
-        val lpar = token("""\(""")
-        val rpar = token("""\)""")
-        val or = token("""\|""")
-        val direction = token("[NEWS]")
-
-        override val tokens = listOf(regexStart, regexEnd, lpar, rpar, or, direction)
-
-        val moves: Parser<Moves> by oneOrMore(direction) map { Moves(it.map { it.text }.joinToString("")) }
-        val alternatives: Parser<List<DungeonNode>> by separatedTerms(optional(parser(::movesOrAlternatives)), or) map {
-            it.map { it ?: Moves("") }
+    companion object {
+        fun opposite(c: Char): Char = when (c) {
+            'N' -> 'S'
+            'E' -> 'W'
+            'S' -> 'N'
+            'W' -> 'E'
+            else -> throw IllegalStateException()
         }
-        val alternativesInBrackets: Parser<Alternatives> by (-lpar and alternatives and -rpar) map { Alternatives(it) }
-        val movesOrAlternatives: Parser<Alternatives> by oneOrMore(moves or alternativesInBrackets) map { Alternatives(it) }
-
-        override val rootParser by (-regexStart * movesOrAlternatives * -regexEnd)
     }
 
-    fun parse(input: String): Alternatives = TreeParser.parseToEnd(input)
-
-    var part2: MutableSet<String> = mutableSetOf()
-
-    fun reduceMoves(acc: Pair<Int, Moves>, char: Char): Pair<Int, Moves> {
-        val (maxDist, moves) = acc
-        val moved = moves.plus(char)
-        if (moved.moves.length >= 1000) {
-            part2.add(moved.moves)
-        }
-        return Math.max(maxDist, moved.moves.length) to moved
-    }
-
-    fun maxDepth(deepest: Int, current: Moves, tree: Alternatives): Int {
-        val firstInTree = tree.options.firstOrNull()
-        when (firstInTree) {
-            null -> return Math.max(deepest, current.moves.length)
-            is Moves -> {
-                val (maxDepthAfterMoving, afterMoving) = firstInTree.moves.fold(deepest to current, ::reduceMoves)
-                return maxDepth(maxDepthAfterMoving, afterMoving, Alternatives(tree.options.drop(1)))
-            }
-            is Alternatives -> {
-                val maxDepthInFirstTree = maxDepth(deepest, current, firstInTree)
-                return maxDepth(maxDepthInFirstTree, current, Alternatives(tree.options.drop(1)))
+    fun bothParts(input: String): Pair<Int, Int> {
+        val stack = Stack<String>()
+        var current = ""
+        var maxDepth = 0
+        val deepRooms= mutableSetOf<String>()
+        for(c in input) {
+            when (c) {
+                'N', 'S', 'E', 'W' -> when {
+                    opposite(c) == current.lastOrNull() -> current = current.dropLast(1)
+                    else -> {
+                        current = current + c
+                        if(current.length >= 1000){
+                            deepRooms.add(current)
+                        }
+                        maxDepth = Math.max(maxDepth, current.length)
+                    }
+                }
+                '(' -> stack.push(current)
+                ')' -> current = stack.pop()
+                '|' -> current = stack.peek()
             }
         }
+        return maxDepth to deepRooms.size
     }
-
-    fun part1(input: String): Int = maxDepth(0, Moves(""), parse(input))
 }
--- src/test/kotlin/nl/kelpin/fleur/advent2018/Day20Test.kt
+++ src/test/kotlin/nl/kelpin/fleur/advent2018/Day20Test.kt
@@ -7,26 +7,19 @@ import org.junit.jupiter.api.Test
 class Day20Test {
 
     @Test
-    fun `TreeParser tokenize`() {
-        assertThat(Day20().TreeParser.tokenizer.tokenize("""^ENWWW(NEEE|SSE(EE|N))${'$'}""").toList()
-                .map { it.text }.joinToString("")).isEqualTo("""^ENWWW(NEEE|SSE(EE|N))${'$'}""")
-    }
-
-    @Test
     fun `sample2 part 1`() {
-        assertThat(Day20().part1("""^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN${'$'}""")).isEqualTo(18)
+        assertThat(Day20().bothParts("""^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN${'$'}""").first).isEqualTo(18)
     }
 
     @Test
     fun `sample1 part 1`() {
-        assertThat(Day20().part1("""^ENWWW(NEEE|SSE(EE|N))${'$'}""")).isEqualTo(10)
+        assertThat(Day20().bothParts("""^ENWWW(NEEE|SSE(EE|N))${'$'}""").first).isEqualTo(10)
     }
 
     @Test
     fun `solution both parts`() {
         val day20 = Day20()
         val input = resourceAsList("day20.txt")[0]
-        assertThat(day20.part1(input)).isEqualTo(3669)
-        assertThat(day20.part2.size).isEqualTo(8369)
+        assertThat(day20.bothParts(input)).isEqualTo(3669 to 8369)
     }
 }
