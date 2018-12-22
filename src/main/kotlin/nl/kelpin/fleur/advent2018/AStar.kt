e6e945312d53c7cdae41591f97197e8e76035629
Fleur Kelpin <f.kelpin@umcg.nl>
2018-12-22 17:49:16 +0100

    refactor(Astar): We don't need the actual path

--- src/main/kotlin/nl/kelpin/fleur/advent2018/AStar.kt
+++ src/main/kotlin/nl/kelpin/fleur/advent2018/AStar.kt
@@ -2,7 +2,7 @@ package nl.kelpin.fleur.advent2018
 
 import java.util.*
 
-interface  Grid<State> {
+interface Grid<State> {
     fun heuristicDistance(from: State, to: State): Int
     fun getNeighbours(state: State): List<State>
     fun moveCost(from: State, to: State): Int
@@ -11,41 +11,23 @@ interface  Grid<State> {
 fun <State> aStarSearch(start: State,
                         finish: State,
                         grid: Grid<State>): Int {
-    fun findCost(currentPos: State, cameFrom: Map<State, State>): Int {
-        var cost = 0
-        var current = currentPos
-        while (cameFrom.containsKey(current)) {
-            val to = cameFrom.getValue(current)
-            cost += grid.moveCost(current, to)
-            current = to
-        }
-        return cost
-    }
-
     val closedVertices = mutableSetOf<State>()
     val costFromStart = mutableMapOf(start to 0)
     val estimatedTotalCost = mutableMapOf(start to grid.heuristicDistance(start, finish))
-    val openVertices = PriorityQueue<State>(compareBy { estimatedTotalCost[it] })
-    val cameFrom = mutableMapOf<State, State>()  // Used to generate path by back tracking
-    openVertices.add(start)
+    val openVertices = PriorityQueue<State>(compareBy { estimatedTotalCost[it] }).also { it.add(start) }
 
     while (openVertices.size > 0) {
         val currentPos = openVertices.poll()
-
-        if (currentPos == finish) {
-            return findCost(currentPos, cameFrom)
-        }
+        if (currentPos == finish) return estimatedTotalCost[currentPos]!!
         closedVertices.add(currentPos)
-
         grid.getNeighbours(currentPos)
                 .filterNot { closedVertices.contains(it) }
                 .forEach { neighbour ->
                     val score = costFromStart.getValue(currentPos) + grid.moveCost(currentPos, neighbour)
                     if (score < costFromStart.getOrDefault(neighbour, Integer.MAX_VALUE)) {
-                        cameFrom.put(neighbour, currentPos)
                         costFromStart.put(neighbour, score)
                         estimatedTotalCost.put(neighbour, score + grid.heuristicDistance(neighbour, finish))
-                        // remove and add because its priority changes
+                        // remove and add because its priority changed
                         openVertices.remove(neighbour)
                         openVertices.add(neighbour)
                     }
